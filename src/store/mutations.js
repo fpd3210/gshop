50e3b24f03c4c6bc117b3ba85edd08756e37d4d0
runn <446933040@qq.com>
2019-08-20 09:22:43 +0800

    添加商品显示跟购物车

--- src/store/mutations.js
+++ src/store/mutations.js
@@ -1,12 +1,20 @@
 /*
 直接更新state的多个方法的对象
  */
+
+import Vue from'vue'
+
 import {
   RECEIVE_ADDRESS,
   RECEIVE_CATEGORYS,
   RECEIVE_SHOPS,
   RECEIVE_USER_INFO,
-  RESET_USER_INFO
+  RESET_USER_INFO,
+  RECEIVE_INFO,
+  RECEIVE_RATINGS,
+  RECEIVE_GOODS,
+  INCREMENT_FOOD_COUNT,
+  DECREMENT_FOOD_COUNT
 } from './mutation-types'
 
 export default {
@@ -27,5 +35,31 @@ export default {
   },
   [RESET_USER_INFO] (state) {
     state.userInfo = {}
+  },
+  [RECEIVE_INFO](state, {info}) {
+    state.info = info
+  },
+  [RECEIVE_RATINGS](state, {ratings}) {
+    state.ratings = ratings
+  },
+  [RECEIVE_GOODS](state, {goods}) {
+    state.goods = goods
+  },
+  [INCREMENT_FOOD_COUNT](state, {food}) {
+    if(!food.count){ //如果没有值添加属性
+      //food.count = 1   //没有数据绑定
+      Vue.set(food,'count',1)  //让新增的属性也有数据绑定
+      state.shopCart.push(food)
+    }else {
+      food.count++
+    }
+  },[DECREMENT_FOOD_COUNT](state, {food}) {
+    console.log('mutations')
+    if (food.count){ //只有有值才去减
+      food.count--
+      if(food.count===0){
+        state.shopCart.splice(state.shopCart.indexOf(food,1))
+      }
+    }
   },
 }
